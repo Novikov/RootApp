48b0d358470ad01eb753ee98c2d6b863a11ec968
Igor Novikov <igor.novikov@umbrellait.com>
2022-07-10 15:14:34 +0300

    root app

--- app/src/main/java/com/w/rootapp/MainActivity.kt
+++ app/src/main/java/com/w/rootapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.w.rootapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.w.libraryapp.DataConsumer
+import com.w.libraryapp.di.DemoSdkApplication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        DemoSdkApplication.init(applicationContext)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val formatter = DataConsumer.greetingsFormatter
+        Log.i("ASDASDSAD", formatter.sayHiWithName())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        DemoSdkApplication.init(applicationContext)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        DemoSdkApplication.close()
     }
 }
